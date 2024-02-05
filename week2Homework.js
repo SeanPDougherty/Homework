59f56dd5eb2bca247cea7e6d4f1b6d9239db3aae
Sean Dougherty <seanssavycoders@gmail.com>
2024-02-04 18:06:48 -0600

    Sumitting week 2 homework

---
+++ week2Homework.js
@@ -0,0 +1,26 @@
+// Pizza Place Homework
+
+// Inside your JS file, create two variables: pizzaPlace and numberOfToppings
+// Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers.
+let pizzaPlace = "Patrick's Pies";
+let numberOfToppings = 11;
+// Print the variables and their types.
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+// Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
+// let number = "My name is ${name} and I am ${age} years old;
+let greeting = `Welcome to ${pizzaPlace}! We have ${numberOfToppings} toppings to choose from.`;
+console.log(greeting);
+
+// Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
+}
+
+// Add, commit, and push your JS file to your GitHub repo.
+// Send your TA a link to your GitHub Homework repo when finished.
